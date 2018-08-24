5a0c777685d1f80380094ff6d5bdd02f6e815eea
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-24 11:17:28 -0500

    added add item to list functionality

--- updated-shopping-list-sean-cameron/index.js
+++ updated-shopping-list-sean-cameron/index.js
@@ -1,13 +1,15 @@
+'use strict';
+
 const STORE = [
-    { name: "apples", checked: false },
-    { name: "oranges", checked: false },
-    { name: "milk", checked: true },
-    { name: "bread", checked: false }
+  { name: "apples", checked: false },
+  { name: "oranges", checked: false },
+  { name: "milk", checked: true },
+  { name: "bread", checked: false }
 ];
 
 function generateHtml(item, index) {
-    let checkedStyle = item.checked ? '.shopping - item__checked' : '';
-    return `<li> class="js-item-index-element"
+  let checkedStyle = item.checked ? '.shopping - item__checked' : '';
+  return `<li class="js-item-index-element"
         data-item-index="${index}">
         <span class="shopping-item ${checkedStyle}">${item.name}</span>
         <div class="shopping-item-controls">
@@ -21,20 +23,35 @@ function generateHtml(item, index) {
       </li>`;
 }
 
+function renderHtml(){
+  const storeHtml = STORE.map((item, index) => generateHtml(item,index)).join('');
+  $('.js-shopping-list').html(storeHtml);
+}
+
+function checkToggle(){
+	$('.js-shopping-list').on('click', '.shopping-item-toggle', function(event){
+		const index = $(this).parents('.js-item-index-element').attr('data-item-index');
+		console.log(index);
+	})
+}
+
+
 function itemAddHandler() {
-    $('#js-shopping-list-form').submit(function (event) {
-        event.preventDefault();
-        const textField = $('.js-shopping-list-entry');
-        let name = textField.val();
-        textField.val("");
-        console.log(name);
-        STORE.push({name, checked:false});
-        console.log(STORE);
-    });
+  $('#js-shopping-list-form').submit(function (event) {
+    event.preventDefault();
+    const textField = $('.js-shopping-list-entry');
+    let name = textField.val();
+    textField.val("");
+		STORE.push({name, checked:false});
+		renderHtml();
+    console.log(STORE);
+  });
 }
 
 function main() {
-    itemAddHandler();
+	itemAddHandler();
+	renderHtml();
+	checkToggle();
 }
 
 $(main);
